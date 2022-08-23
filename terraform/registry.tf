e9bea716674b342753d12bf788104a803863fcb6
rafael moreira <rafael_mg22@hotmail.com>
2022-08-22 21:33:10 -0300

    add artifactory registry and files to build app flask

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,4 +1,5 @@
 resource "google_artifact_registry_repository" "repo" {
+  provider      = google-beta
   location      = "us-central1"
   repository_id = "api-flask"
   description   = "Repo for flask application"
